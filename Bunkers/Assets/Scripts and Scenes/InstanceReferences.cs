d9755d87afc2dab947257a8834de8524161a9ce2
Louis Urquhart <louisurquhart@gmail.com>
2024-12-29 21:01:52 +0000

    encapsulation of instance references

--- Bunkers/Assets/Scripts and Scenes/InstanceReferences.cs
+++ Bunkers/Assets/Scripts and Scenes/InstanceReferences.cs
@@ -13,17 +13,49 @@ public class InstanceReferences : MonoBehaviour
         Instance = this;
     }
 
-    // Timer text references
-    public TMP_Text PlayerTimerText;
-    public TMP_Text AITimerText;
+    // Timer text references. They're encapsulated so they are read only except for by the inspector with the private encapsulated variables serialized meaning inspector can still modify them.
+    [SerializeField] private TMP_Text playerTimerText;
+    [SerializeField] private TMP_Text aiTimerText;
 
-    // Additive game menu references
-    public GameObject EndMenuUI;
-    public GameObject PauseMenuUI;
-    
-    // Class instance references
-    public TimerScript TimerScriptInstance;
-    public GeneralBackgroundLogic GeneralBackgroundLogicInstance;
+    public TMP_Text PlayerTimerText // Read only property
+    {
+        get { return playerTimerText; }
+    }
 
+    public TMP_Text AITimerText // Read only property
+    {
+        get { return aiTimerText; }
+    }
+
+    // Additive game menu references. They're encapsulated so they are read only except for by the inspector with the private encapsulated variables serialized meaning inspector can still modify them.
+
+    [SerializeField] private GameObject endMenuUI;
+    [SerializeField] private GameObject pauseMenuUI;
+
+    public GameObject EndMenuUI // Read only property
+    {
+        get { return endMenuUI; }
+    }
+
+    public GameObject PauseMenuUI // Read only property
+    {
+        get { return pauseMenuUI; }
+    }
+
+
+    // Class instance references. They're encapsulated so they are read only except for by the inspector with the private encapsulated variables serialized meaning inspector can still modify them.
+
+    [SerializeField] private TimerScript timerScriptInstance;
+    [SerializeField] private GeneralBackgroundLogic generalBackgroundLogicInstance;
+
+    public TimerScript TimerScriptInstance // Read only property
+    {
+        get { return timerScriptInstance; }
+    }
+
+    public GeneralBackgroundLogic GeneralBackgroundLogicInstance // Read only property
+    {
+        get { return generalBackgroundLogicInstance; }
+    }
 }
 
