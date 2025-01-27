b062cf85a7fdbc1da4ec57e5213a1f047c34d839
Louis Urquhart <louisurquhart@gmail.com>
2025-01-27 22:35:44 +0000

    minor changes to options menu array names

--- Bunkers/Assets/Scripts + Scenes/MenuAndNavigation/SceneNavigationFunctions.cs
+++ Bunkers/Assets/Scripts + Scenes/MenuAndNavigation/SceneNavigationFunctions.cs
@@ -1,8 +1,4 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class SceneNavigationFunctions : MonoBehaviour
